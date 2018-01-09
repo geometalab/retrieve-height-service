73f32d7acbbc1a1575ffc43bab1251e4f307f497
Nicola Jordan <nic@hixi.ch>
2018-01-09 14:08:57 +0100

    make get_data documentation; this will never be executed this way, since it now needs a login

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-# wget -L --random-wait --timestamping --load-cookies ~/.cookies --save-cookies ~/.cookies -i data_list_2018-01-09.txt
-# only execute this once, therefore commented
 
-# normal execution
-# wget --random-wait -i data_list.txt --timestamping
-# execute in parallel, especially when havin a fast internet connection quite usefull
-# cat ../data_list.txt | parallel --gnu "wget --random-wait --timestamping {}"
+echo "You need an user account to download data; Please create on in order to use this downloader at https://urs.earthdata.nasa.gov."
+
+# wget --user=$USER --ask-password --continue -L --random-wait --timestamping --load-cookies ~/.cookies --save-cookies ~/.cookies -i data_list_2018-01-09.txt
+# execute in parallel, especially when havin a fast internet connection
+# cat ../data_list_2018-01-09.txt | parallel --gnu "wget --user=$USER --ask-password --continue -L --random-wait --timestamping --load-cookies ~/.cookies --save-cookies ~/.cookies {}"
+
+echo ""
+echo "
+##############
+
+wget --user=$USER --ask-password --continue -L --random-wait --timestamping --load-cookies ~/.cookies --save-cookies ~/.cookies -i data_list_2018-01-09.txt
+
+**WARNING**: before continuing, check that 
+`cat data_list_2018-01-09.txt |  wc -l` and `ls -l *.zip | wc -l` is the same.
+
+ie. `cat data_list_2018-01-09.txt |  wc -l -eq ls -l *.zip | wc -l`
+
+Next steps:
 
 # unzip hgts
 unzip *.zip
@@ -13,5 +25,6 @@ unzip *.zip
 # build worldfile
 gdalbuildvrt world.vrt *.hgt
 
-# "healthcheck" of worldfile
+# healthcheck of worldfile
 gdalinfo -mm world.vrt
+"
