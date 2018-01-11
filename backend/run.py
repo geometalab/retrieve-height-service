d9e1d0ddf0e995993370c1a01fac7a2e39ba1a41
Nicola Jordan <nic@hixi.ch>
2018-01-11 10:33:52 +0100

    listen to debug environment variable

--- backend/run.py
+++ backend/run.py
@@ -3,11 +3,12 @@ Created on 230315
 Created by Phua Joon Kai Eugene
 Last Modification on 050515
 """
+import os
 from webapp import app
 import unittest
 import flask_testing
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=bool(os.environ.get('DEBUG')))
     app.config['TESTING'] = True
     unittest.main()
