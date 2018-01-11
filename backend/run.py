83b835c8ea163bf44568a38f6f45ca23893e2214
Nicola Jordan <nic@hixi.ch>
2018-01-11 10:49:14 +0100

    add suport for sentry

--- backend/run.py
+++ backend/run.py
@@ -7,8 +7,12 @@ import os
 from webapp import app
 import unittest
 import flask_testing
+from raven.contrib.flask import Sentry
+
+sentry = Sentry()  # automatically reads 'SENTRY_DSN' if set
 
 if __name__ == '__main__':
+    sentry.init_app(app)
     app.run('0.0.0.0', port=5000, debug=bool(os.environ.get('DEBUG')))
     app.config['TESTING'] = True
     unittest.main()
